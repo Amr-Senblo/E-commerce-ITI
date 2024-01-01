c26c36f55d87c59b264a34f6a9b6f468ca93380e
EmadERagheb <emaderagheb@gmail.com>
2024-01-01 09:53:41 +0200

    create routing for different components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
-
-import { CarouselComponent } from '../components/carousel/carousel.component';
-import { ProductComponent } from '../components/product/product.component';
-import { FooterComponent } from '../components/footer/footer.component';
-import { HeaderComponent } from '../components/header/header.component';
-import { ProfileComponent } from '../components/profile/profile.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    CarouselComponent,
-    ProductComponent,
-    FooterComponent,
-    HeaderComponent,
-    ProfileComponent
-  ],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'E-commerce-ITI';
-
-  images = [
-    {
-      imgSrc:
-        'https://images.unsplash.com/photo-1703774626005-9eb89bd30df4?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imgAlt: 'photo',
-    },
-    {
-      imgSrc:
-        'https://images.unsplash.com/photo-1682686579688-c2ba945eda0e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imgAlt: 'photo',
-    },
-    {
-      imgSrc:
-        'https://plus.unsplash.com/premium_photo-1664382465607-420346d391bd?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imgAlt: 'photo',
-    },
-  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from '../components/product-list/product-list.component';
+import { ProductDetailsComponent } from '../components/product-details/product-details.component';
+import { ErrorPageComponent } from '../components/error-page/error-page.component';
+import { CartComponent } from '../components/cart/cart.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { MainLayoutComponent } from '../layouts/main-layout/main-layout.component';
+import { HomeComponent } from '../components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'Home', component: HomeComponent },
+      { path: '', redirectTo: 'Home', pathMatch: 'full' },
+      { path: 'Category/:categoryId', component: ProductListComponent },
+      { path: 'Category/:categoryId/:id', component: ProductDetailsComponent },
+      { path: 'Cart', component: CartComponent },//Add Guard
+    ],
+  },
+  { path: 'Profile/:id', component: ProfileComponent },//Add Guard
+  { path: '**', component: ErrorPageComponent },
+];
