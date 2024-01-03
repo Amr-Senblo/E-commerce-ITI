4b95860892d5d036192a78305ff3c63119e5b331
shroukyahia <shrouk.yahia.3@gmail.com>
2024-01-02 21:21:34 -0800

    d

---
+++ product.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, concat, map } from 'rxjs';
+import { IProduct } from '../models/iproduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  DB="http://localhost:3000/products";
+
+  constructor(private http:HttpClient) { }
+  getProducts()
+  {
+    return this.http.get<IProduct[]>(this.DB);
+  }
+  getProduct(id:number)
+  {
+    let url= `${this.DB}/${id}`;
+    return this.http.get<IProduct>(url);
+  }
+  createProduct(data:{}){
+    return this.http.post<IProduct>(this.DB,data);
+  }
+  updateProduct(id:number,data:{})
+  {
+    let url= `${this.DB}/${id}`;
+    return this.http.put<IProduct>(url,data);
+  }
+  deleteProduct(id:number)
+  {
+    let url= `${this.DB}/${id}`;
+    return this.http.delete<IProduct>(url);
+  }
+}
