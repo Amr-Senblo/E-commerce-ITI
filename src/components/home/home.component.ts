15f95e24ca2877c402d78e6b15f06e3ef24a6d65
EmadERagheb <emaderagheb@gmail.com>
2024-01-04 22:45:11 +0200

    after mariam updates

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { CategoriesComponent } from '../categories/categories.component';
-
-import { customHomeService } from '../../services/customHome.Service';
-
-
 import { HttpClientModule } from '@angular/common/http';
 import { IProduct } from '../../models/iproduct';
 import { ProductsArrayComponent } from '../products-array/products-array.component';
 import { Image } from '../../models/image';
 import { ICategory } from '../../models/icategory';
 import { CategoryService } from '../../services/category.service';
-
-
 import { ProductService } from '../../services/product.service';
 import { SliderComponent } from '../slider/slider.component';
-
 
 @Component({
   selector: 'app-home',
@@ -23,51 +16,62 @@ import { SliderComponent } from '../slider/slider.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 
-
-
+  imports: [
+    CarouselComponent,
+    CategoriesComponent,
+    HttpClientModule,
+    ProductsArrayComponent,
+    SliderComponent,
+  ],
+  providers: [ProductService, CategoryService],
+})
 export class HomeComponent implements OnInit {
-  images: Image[] = []
-
-  imports: [CarouselComponent, CategoriesComponent, HttpClientModule, ProductsArrayComponent, SliderComponent],
-  providers: [ProductService, CategoryService]
-
-export class HomeComponent implements OnInit {
-  images: Image[] = [{ imgSrc: 'https://m.media-amazon.com/images/I/714qCf4ZqGL.SX3000.jpg', imgAlt: 'cover' }]
+  images: Image[] = [
+    {
+      imgSrc: 'https://m.media-amazon.com/images/I/714qCf4ZqGL.SX3000.jpg',
+      imgAlt: 'cover',
+    },
+  ];
 
   allCategories: ICategory[] = [];
   mobileCatg4: IProduct[] = [];
   smartWatch4: IProduct[] = [];
   laptops4: IProduct[] = [];
 
-  constructor(public productService: ProductService, public categoryServise: CategoryService) { }
+  constructor(
+    public productService: ProductService,
+    public categoryServise: CategoryService
+  ) {}
 
   ngOnInit(): void {
-
     this.productService.getProduct4(1).subscribe({
       next: (value) => {
         this.mobileCatg4 = value;
       },
-      error: (err) => console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
     this.productService.getProduct4(2).subscribe({
       next: (value) => {
         this.smartWatch4 = value;
       },
-      error: (err) => console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
     this.productService.getProduct4(3).subscribe({
       next: (value) => {
         this.laptops4 = value;
       },
-      error: (err) => console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
     this.categoryServise.getCategories().subscribe({
       next: (value) => {
-        this.allCategories = value
+        this.allCategories = value;
         for (let category of this.allCategories)
-          this.images.push({ imgSrc: category.imageCover, imgAlt: category.name })
+          this.images.push({
+            imgSrc: category.imageCover,
+            imgAlt: category.name,
+          });
       },
-      error: (err) => console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
   }
 }
