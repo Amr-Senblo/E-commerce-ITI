4192601fb5f17ccf5dd0b45224167f4ce75b15d9
Mariam732 <mariamabdalla26564@gmail.com>
2024-01-04 21:09:05 +0200

    added product details 1

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,53 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { CategoriesComponent } from '../categories/categories.component';
-import { ProductComponent } from '../product/product.component';
+import { customHomeService } from '../../services/customHome.Service';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductService } from '../../services/product.service';
 import { IProduct } from '../../models/iproduct';
+import { ProductsArrayComponent } from '../products-array/products-array.component';
+import { Image } from '../../models/image';
+import { ICategory } from '../../models/icategory';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [CarouselComponent, CategoriesComponent, ProductComponent, HttpClientModule],
-  providers: [ProductService],
-
+  imports: [CarouselComponent, CategoriesComponent, HttpClientModule, ProductsArrayComponent],
+  providers: [customHomeService, CategoryService]
 })
 export class HomeComponent implements OnInit {
-  images = [
-    {
-      imgSrc:
-        'https://images.unsplash.com/photo-1703774626005-9eb89bd30df4?q=80&w=1852&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imgAlt: 'photo',
-    },
-    {
-      imgSrc:
-        'https://images.unsplash.com/photo-1682686579688-c2ba945eda0e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imgAlt: 'photo',
-    },
-    {
-      imgSrc:
-        'https://plus.unsplash.com/premium_photo-1664382465607-420346d391bd?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      imgAlt: 'photo',
-    },
-  ];
+  images: Image[] = []
+  allCategories: ICategory[] = [];
+  mobileCatg4: IProduct[] = [];
+  smartWatch4: IProduct[] = [];
+  laptops4: IProduct[] = [];
+  constructor(public productService: customHomeService, public categoryServise: CategoryService) { }
 
-  constructor(private myService: ProductService) { }
-
-  AllProducts: IProduct[] = [];
   ngOnInit(): void {
-
-    this.myService.getProducts().subscribe((data: IProduct[]) => {
-      this.AllProducts = data.slice(0, 8);
-      console.log(this.AllProducts);
-      
-    });
-
-
-
+    //getProduct4 () ??????
+    this.productService.getProduct4(1).subscribe({
+      next: (value) => {
+        this.mobileCatg4 = value;
+      },
+      error: (err) => console.log(err)
+    })
+    this.productService.getProduct4(2).subscribe({
+      next: (value) => {
+        this.smartWatch4 = value;
+      },
+      error: (err) => console.log(err)
+    })
+    this.productService.getProduct4(3).subscribe({
+      next: (value) => {
+        this.laptops4 = value;
+      },
+      error: (err) => console.log(err)
+    })
+    this.categoryServise.getCategories().subscribe({
+      next: (value) => {
+        this.allCategories = value
+        for (let category of this.allCategories)
+          this.images.push({ imgSrc: category.imageCover, imgAlt: category.name })
+      },
+      error: (err) => console.log(err)
+    })
   }
-
-
 }
