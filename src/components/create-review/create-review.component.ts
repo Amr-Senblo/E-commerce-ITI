dddfcc9d560d74824c7dba48f2eb97b03deee24a
DESKTOP-STHR72D\Amr Senblo <amrsenblo@gmail.com>
2024-01-12 00:16:33 +0200

    small edits after merge

--- src/components/create-review/create-review.component.ts
+++ src/components/create-review/create-review.component.ts
@@ -3,7 +3,12 @@ import { IUser } from '../../models/iuser';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ReviewService } from '../../services/review.service';
 import { IReview } from '../../models/ireview';
@@ -12,74 +17,79 @@ import { IReview } from '../../models/ireview';
   selector: 'app-create-review',
   standalone: true,
   imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
-  providers:[UserService],
+  providers: [UserService],
   templateUrl: './create-review.component.html',
-  styleUrl: './create-review.component.css'
+  styleUrl: './create-review.component.css',
 })
-export class CreateReviewComponent implements OnInit{
-  @Input() userID!:number;
-  @Input() productID!:number;
-  @Output() reviewCreated: EventEmitter<IReview[]> = new EventEmitter<IReview[]>();
+export class CreateReviewComponent implements OnInit {
+  @Input() userID!: number;
+  @Input() productID!: number;
+  @Output() reviewCreated: EventEmitter<IReview[]> = new EventEmitter<
+    IReview[]
+  >();
 
-  user!:IUser;
+  user!: IUser;
   myForm: FormGroup;
-  @Output() newReview!:IReview;
+  @Output() newReview!: IReview;
 
-  reviewsOfProduct:IReview[]=[];
+  reviewsOfProduct: IReview[] = [];
 
-  constructor(private userService:UserService,private reviewService:ReviewService,private route:ActivatedRoute){
-    this.route.params.subscribe( (params) => {
-          this.productID = params['id'];  //get product id
-          console.log(this.productID)
-    })
-    this.myForm= new FormGroup(
-      {
-        comment:new FormControl(null, Validators.required),
-        user:new FormControl(this.userID),
-        productId:new FormControl(this.productID),
-        rating:new FormControl(4, [Validators.required, Validators.min(1),Validators.max(5)]),//static rating until handling rating star
-      });
+  constructor(
+    private userService: UserService,
+    private reviewService: ReviewService,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe((params) => {
+      this.productID = params['id']; //get product id
+      console.log(this.productID);
+    });
+    this.myForm = new FormGroup({
+      comment: new FormControl(null, Validators.required),
+      user: new FormControl(this.userID),
+      productId: new FormControl(this.productID),
+      rating: new FormControl(4, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+      ]), //static rating until handling rating star
+    });
   }
 
   ngOnInit(): void {
     this.userService.getUser(this.userID).subscribe({
-      next:(data)=> {
-        this.user=data
+      next: (data) => {
+        this.user = data;
         console.log(this.user);
       },
-      error:()=>console.log("error")
-    })
+      error: () => console.log('error'),
+    });
   }
-  checkUser(){
-    if(this.user === null)
-    return ;
+  checkUser() {
+    if (this.user === null) return;
   }
-  send(){
-      let comment=this.myForm.controls['comment'].value;
-      let user=this.userID;
-      let productId= +this.productID;
-      let rating=this.myForm.controls['rating'].value;
+  send() {
+    let comment = this.myForm.controls['comment'].value;
+    let user = this.userID;
+    let productId = +this.productID;
+    let rating = this.myForm.controls['rating'].value;
 
-      let newReview={comment, user, productId, rating};
+    let newReview = { comment, user, productId, rating };
 
-      this.reviewService.createReview(newReview).subscribe(
-        {
-          next: (createdReview) => {
-            this.reviewService.getReviewsOfProduct(this.productID).subscribe({
-              next:(data)=>{
-                this.reviewsOfProduct=data;
-                console.log(this.reviewsOfProduct);
+    this.reviewService.createReview(newReview).subscribe({
+      next: (createdReview) => {
+        this.reviewService.getReviewsOfProduct(this.productID).subscribe({
+          next: (data: IReview[]) => {
+            this.reviewsOfProduct = data;
+            console.log(this.reviewsOfProduct);
 
-                this.reviewCreated.emit(this.reviewsOfProduct); // Emit the new review
-              }
-            });
-            this.myForm.reset();
+            this.reviewCreated.emit(this.reviewsOfProduct); // Emit the new review
           },
-          error: () => {
-            console.log("error");
-          }
-        }
-      );
+        });
+        this.myForm.reset();
+      },
+      error: () => {
+        console.log('error');
+      },
+    });
   }
-
 }
