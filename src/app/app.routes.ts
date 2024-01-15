4922b812705d4a5a6e6cbc3025f8f62760e5c187
DESKTOP-STHR72D\Amr Senblo <amrsenblo@gmail.com>
2024-01-16 00:03:59 +0200

    main ui of user-profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-
 import { ErrorPageComponent } from '../components/error-page/error-page.component';
 import { CartComponent } from '../components/cart/cart.component';
 import { ProfileComponent } from '../components/profile/profile.component';
@@ -15,7 +14,7 @@ import { Component } from '@angular/core';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { ProductDetailsContainerComponent } from '../components/product-details-container/product-details-container.component';
 import { ProductListComponent } from '../components/product-list/product-list.component';
-
+import { UserProfileComponent } from '../components/user-profile/user-profile.component';
 
 export const routes: Routes = [
   {
@@ -25,13 +24,15 @@ export const routes: Routes = [
       { path: 'Home', component: HomeComponent },
       { path: '', redirectTo: 'Home', pathMatch: 'full' },
 
-
       { path: 'category/:categoryId', component: ProductListComponent },
-      { path: 'Category/:categoryId/:id', component: ProductDetailsContainerComponent },
+      {
+        path: 'Category/:categoryId/:id',
+        component: ProductDetailsContainerComponent,
+      },
       { path: 'Cart/:id', component: CartComponent }, //Add Guard
       { path: 'Search/:word', component: FilterComponent },
-      { path: 'AboutUs', component: AboutUsComponent }
-
+      { path: 'AboutUs', component: AboutUsComponent },
+      { path: 'myprofile', component: UserProfileComponent },
     ],
   },
   { path: 'CreateProduct', component: CreateProductComponent },
