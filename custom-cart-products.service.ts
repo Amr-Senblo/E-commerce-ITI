cb70427af8596ff0e0638871ec4da8211a3834a9
shroukyahia <shrouk.yahia.3@gmail.com>
2024-01-15 23:37:38 -0800

    x

---
+++ custom-cart-products.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IProduct } from '../models/iproduct';
+import { IproductBuyed } from '../models/iproduct-buyed';
+import { ICart } from '../models/icart';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomCartService {
+  urlProduct = `${environment.apiUrl}/products`;
+  urlcart = ` ${environment.apiUrl}/carts`;
+  constructor(private http: HttpClient) { }
+  getCartProducts(productsIds: string) {
+    return this.http.get<IProduct[]>(`${this.urlProduct}?${productsIds}`)
+  }
+  editCartProducts(CartId: number, productsArray: IproductBuyed[]) {
+    return this.http.patch<ICart>(`${this.urlcart}/${CartId}`, { "products": productsArray })
+  }
+  editCartTotalPrice(CartId: number, totalPrice: number) {
+    return this.http.patch<ICart>(`${this.urlcart}/${CartId}`, { "totalPrice": totalPrice })
+  }
+}
--- product.service.ts
+++ product.service.ts
@@ -1,35 +1,51 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, concat, map } from 'rxjs';
 import { IProduct } from '../models/iproduct';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  DB="http://localhost:3000/products";
+  DB = `${environment.apiUrl}/products`;
 
-  constructor(private http:HttpClient) { }
-  getProducts()
-  {
+  constructor(private http: HttpClient) { }
+  getProducts() {
     return this.http.get<IProduct[]>(this.DB);
   }
-  getProduct(id:number)
-  {
-    let url= `${this.DB}/${id}`;
+  getProduct(id: number) {
+    let url = `${this.DB}/${id}?_embed=reviews`;
     return this.http.get<IProduct>(url);
   }
-  createProduct(data:{}){
-    return this.http.post<IProduct>(this.DB,data);
+  
+
+  createProduct(data: {}) {
+    return this.http.post<IProduct>(this.DB, data);
   }
-  updateProduct(id:number,data:{})
-  {
-    let url= `${this.DB}/${id}`;
-    return this.http.put<IProduct>(url,data);
+  updateProduct(id: number, data: {}) {
+    let url = `${this.DB}/${id}`;
+    return this.http.put<IProduct>(url, data);
   }
-  deleteProduct(id:number)
-  {
-    let url= `${this.DB}/${id}`;
+  updateProductQuantity(id: number, newQuantity: number) {
+    let url = `${this.DB}/${id}`;
+    return this.http.patch<IProduct>(url, { "quantity": newQuantity });
+  }
+  deleteProduct(id: number) {
+    let url = `${this.DB}/${id}`;
     return this.http.delete<IProduct>(url);
   }
+
+  getProductsOfCategory(categoryId: number) {
+    let url = `${this.DB}?category=${categoryId}`;
+    return this.http.get<IProduct[]>(url)
+  }
+
+  getProduct4(categoryId: number) {
+    let url = `${this.DB}?category=${categoryId}&_page=1&_limit=4`;
+    return this.http.get<IProduct[]>(url)
+  }
+  // getPaginateProduct(categoryId: number, itemsNum: number) {
+  //   let url =`${this.DB}?category=${categoryId}&_page=1&_limit=${itemsNum}`;
+  //   return this.http.get<IProduct[]>(url)
+  // }
 }
